109bdfeea6abe7a89f4777cf21ef17b486240a6a
joserobjr <jose.rob.jr@gmail.com>
2016-11-09 05:21:52 -0300

    Updated to Minecraft 1.10

--- src/main/java/com/mcf/davidee/guilib/GuiLibrary.java
+++ src/main/java/com/mcf/davidee/guilib/GuiLibrary.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import java.util.Arrays;
 
-@Mod(modid = "guilib", name = "GUI Library", version = "1.9.4.0")
+@Mod(modid = "guilib", name = "GUI Library", version = "1.10.0.0")
 public class GuiLibrary {
 
 
